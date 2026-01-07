9caf2adee5256b11a892cd097f8285ab9643b4ac
Sudarshan1397 <91606871+Sudarshan1397@users.noreply.github.com>
2026-01-07 14:31:31 +0530

    Add files via upload

---
+++ Mathematical operations.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Addition is",a+b)
+print("Subtraction is",a-b)
+print("Multiplication is",a*b)
+print("Division is",a/b)
